afdfe3609e8b85377e9d8501b9d7335bfe387009
taku1417 <taku1417java@gmail.com>
2020-12-27 14:48:35 +0900

    tp-above完成(0.2.0完成)
    アイテム吸い寄せは0.5.0に持っていきました
    とりあえず0.2.0が完成

--- src/tk/seichi/TPAboveCom.java
+++ src/tk/seichi/TPAboveCom.java
@@ -17,18 +17,47 @@ public class TPAboveCom implements CommandExecutor {
 		double dis = 20;
 		switch(args.length) {
 		case 0:
+			//コマンドを入力したのがプレイヤーかどうかチェック
 			if(!(sender instanceof Player)) return false;
 			//上に飛ばすために座標取得
 			Player player = (Player)sender;
 			Location y = player.getLocation();
 			y.add(0,dis,0);
+			//飛ばす(20固定)
 			player.teleport(y, PlayerTeleportEvent.TeleportCause.PLUGIN);
-		}
-		//case 1 で使う
-		/*int i = Integer.parseInt(args[0]);
-		int dis = i;*/
 
+			break;
+
+		case 1:
+			//コマンドを入力したのがプレイヤーかどうかチェック
+			if(!(sender instanceof Player)) return false;
+			//文字列として入力された数値をdoubleに変換(例外処理)、代入
+			double input = -1;
+			try {
+				input = Integer.parseInt(args[0]);
+			}
+			catch (NumberFormatException ex) {
+				sender.sendMessage(" "+args[0]+" は無効です。正しい数字を指定してください。");
+				return true;
+			}
+			if(input <= 0) {
+				sender.sendMessage("0以下の数字は指定できません。正の数を指定して下さい。");
+				return true;
+			}
+			dis = input;
+			//上に飛ばすために座標取得
+			player = (Player)sender;
+			y = player.getLocation();
+			y.add(0,dis,0);
+			//飛ばす(入力値によって変わる
+			player.teleport(y, PlayerTeleportEvent.TeleportCause.PLUGIN);
+			break;
+
+		default:
+			sender.sendMessage("コマンドが不正です。確認してください。");
+			break;
+
+		}
 		return true;
 	}
-
 }
